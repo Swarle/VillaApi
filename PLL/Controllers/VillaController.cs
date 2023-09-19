d653a4f102d2b79a55d73047c9b89e746712bc95
Swarle <bogdanvalman@gmail.com>
2023-09-20 00:25:25 +0300

    Add layers, db context and provide serilog

---
+++ PLL/Controllers/VillaController.cs
@@ -0,0 +1,29 @@
+﻿using DataLayer.Context;
+using DataLayer.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PLL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+        private readonly ApplicationContext _context;
+
+        public VillaController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<VillaController>
+        [HttpGet]
+        public IEnumerable<Villa> Get()
+        {
+            return _context.Villa.ToList();
+        }
+
+        
+    }
+}
